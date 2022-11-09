3b983c1518cdb1fedb0f9a7b0f097e6b7cc9ddf3
Noah Heckenlively <heckenna@rose-hulman.edu>
2022-11-08 19:33:50 -0600

    Perceptron Class added
    Also, normalized data elements. Will need to revisit the way I normalized it.

--- data_opener.py
+++ data_opener.py
@@ -10,6 +10,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
+
 # This may not quite be the function we want, be we will roll with it for now
 def train_test_val_split(data, target = "cardio"):
     # Target is cardio
@@ -34,14 +35,33 @@ def get_data(filename = "cardio_train.csv"):
     
     # We have 2 ordinal variables: Cholesterol and Glucose.
     # Going to use one hot encoding on them
-    df_dummied = pd.get_dummies(df, columns = ["cholesterol", "gluc", "gender"], drop_first = True)
+    df = pd.get_dummies(df, columns = ["cholesterol", "gluc", "gender"], drop_first = True)
     
     # Renames at least the gender column to have meaning. 
     # Could do same to chol and gluc if desired
-    df_dummied.rename(columns = {"gender_2":"is_man"}, inplace = True)
+    df.rename(columns = {"gender_2":"is_man"}, inplace = True)
     
-    return df_dummied
+    df = normalize_data(df)
+    
+    # Max corr is .5, so correlation not going to be used for feature selection
+    #corr_matrix = data.corr()
+    #print(corr_matrix)
+    
+    return df
 
+# TODO Need to go back and normalize based on only training/test data
+def normalize_data(data):
+    # For each column in df, make it be between 0 and 1
+    
+    for column_name in data.columns:
+        col = data[column_name]
+        col_max = max(col)
+        
+        data[column_name] = data[column_name]/col_max
+        
+    return data
+    
+    
 
 #X_train, X_test, X_val, y_train, y_test, y_val = train_test_val_split(df_dummied)
 
--- main.py
+++ main.py
@@ -8,10 +8,30 @@ Created on Tue Nov  8 17:50:10 2022
 import numpy as np
 import pandas as pd
 import data_opener
-
+from perceptron import *
 
 data = data_opener.get_data()
+
+# Max corr is .5, so correlation not going to be used for feature selection
+corr_matrix = data.corr()
+print(corr_matrix)
+
 X_train, X_test, X_val, y_train, y_test, y_val = data_opener.train_test_val_split(data)
 
 
 
+size = 13
+h_num = 2000
+
+# Running on top 2000 rows currently, just to save time with debugging
+perc = Perceptron(size = size, epochs = 10)
+perc.train_classifier(X_train.head(h_num), y_train.head(h_num))
+
+pred_train = perc.predict_data(X_train)
+pred_test = perc.predict_data(X_test)
+
+acc_train = sum(pred_train == y_train)/len(y_train)
+acc_test = sum(pred_test == y_test)/len(y_test)
+
+print(acc_train)
+print(acc_test)
