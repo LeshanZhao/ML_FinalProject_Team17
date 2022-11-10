dd832cb7c8d16897c43a3d98c2b7fccb08a5b97c
Noah Heckenlively <heckenna@rose-hulman.edu>
2022-11-10 10:57:57 -0600

    Created two files to work from

---
+++ layer.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 10:56:14 2022
+
+@author: heckenna
+"""
+import perceptron
+
+
+class Layer:
+    def __init__(self):
+        pass
---
+++ mlp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 10:55:12 2022
+
+@author: heckenna
+"""
+import perceptron
+import layer
+
+class MLP:
+    def __init__(self):
+        pass
