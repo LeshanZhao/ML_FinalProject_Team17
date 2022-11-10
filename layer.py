9ff02a2f8ff0ae7a4bc7a126f25317687ac3df4a
Noah Heckenlively <heckenna@rose-hulman.edu>
2022-11-10 11:09:31 -0600

    Added Predict method to Layer class

--- layer.py
+++ layer.py
@@ -5,8 +5,28 @@ Created on Thu Nov 10 10:56:14 2022
 @author: heckenna
 """
 import perceptron
-
+import numpy as np
 
 class Layer:
-    def __init__(self):
-        pass
+    
+    def __init__(self, perceptron_list, weights = None):
+        self.perceptron_list = perceptron_list
+        
+        if weights != None:    
+            self.weight_matrix = weights
+        else:
+            pass
+        
+    def predict(self, inpt):
+        """
+        # Gets output of hidden layer based on input inpt
+        output = []
+        
+        for w_row, perc in zip(weight_matrix, perceptron_list):
+            output.append(perc.predict(np.dot(w_row, inpt)))
+            
+        return output
+        #"""
+        return [perc.predict(np.dot(w_row, inpt)) for w_row, perc in zip(weight_matrix, perceptron_list)]        
+        
+        
