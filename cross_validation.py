21fd8054b91237973fe45edecccd4e07b1e28e30
MaramJehad9 <93616918+MaramJehad9@users.noreply.github.com>
2022-11-23 19:45:23 -0600

    Update cross_validation.py

--- cross_validation.py
+++ cross_validation.py
@@ -13,8 +13,7 @@ class cross_validation:
         self.y = dataset.iloc[:,[-1]]
         self.X = dataset.drop(self.y,axis = 1)
 
-        
-    def train_test_split(self, features, targ, test_size=0.2, random_state=9208):
+    def train_test_split(self, features, targ, test_size=0.2):
         #find the train percentage
         train_percentage = (1 - test_size) * 100
         test_percentage = test_size * 100
@@ -27,28 +26,24 @@ class cross_validation:
         X_test, y_test =  features[:, test_data_size], targ[:,test_data_size]
 
         return X_train, X_test, y_train, y_test
-
-    def cross_val_score(self, estimator , X , y , cv=0 ): 
-        folds = self.Kfold(cv)
-        conf_matrix = [cv] 
-        for fold in folds: 
-            conf_matrix.append(self.confusion_matrix())
-        return
+   
 
     def createFold(self, start_index, fold_size):
-        return self.dataset.iloc[start_index: fold_size,:]
+        return self.dataset.iloc[start_index:, fold_size:]
 
-    def Kfold (self, n_splits):
+    def Kfold (self, n_splits, shuffle = True):
         # Use n_splits to consider the number of folds 
         # Create each fold from the data starting from index i*fold_size until (i * fold_size + fold_size)
         # For Ex: fold_size is 50 
         # The 1st Fold will start from 0*50 until (0 * 50 + 50) in data 
         # The 2nd Fold from 1 * 50 until (1 * 50 + 50 )  
-           
         fold_size = len(self.dataset)/n_splits
         folds = [] 
         for i in n_splits:
-            folds.append(self.createFold(i*fold_size, fold_size))
+            if shuffle == True: 
+                self.dataset = self.dataset.sample(frac = 1)
+
+            folds.append(self.createFold(i*fold_size, fold_size), shuffle)
         return folds
 
 
@@ -75,5 +70,13 @@ class cross_validation:
         Precision = TP / (TP + FP)
         Recall = TP / (TP + FN)
         Accuracy = (TP + TN) / (TP + TN + FP + FN)
-        return Precision, Recall, Accuracy
-  
+        ErrorRate = 1 - Accuracy
+        return Precision, Recall, Accuracy, ErrorRate
+
+def cross_val_score(self, cv=2, shuffle=True): 
+        if shuffle == True: 
+            folds = self.Kfold(cv, shuffle)
+        conf_matrix = [cv] 
+        for fold in folds: 
+            conf_matrix.append(self.confusion_matrix())
+        return conf_matrix
